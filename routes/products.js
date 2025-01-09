3f3d8c2a46c1548286e98a67ef0e8b01f4130081
fabrigabriell <150145958+fabrigabriell@users.noreply.github.com>
2025-01-09 12:16:22 -0300

    Add files via upload

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const fs = require('fs').promises;
 const path = './data/productos.json';
 
-
 router.get('/', async (req, res) => {
   const { limit } = req.query;
   try {
@@ -18,13 +17,12 @@ router.get('/', async (req, res) => {
   }
 });
 
-
 router.get('/:pid', async (req, res) => {
   const { pid } = req.params;
   try {
     const data = await fs.readFile(path, 'utf8');
     const products = JSON.parse(data);
-    const product = products.find(p => p.id === pid);
+    const product = products.find(p => p.id === parseInt(pid));
     if (!product) return res.status(404).send('Producto no encontrado');
     res.json(product);
   } catch (err) {
@@ -33,68 +31,55 @@ router.get('/:pid', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const { title, description, code, price, status, stock, category, thumbnails } = req.body;
-
-    if (!title || !description || !code || !price || !status || !stock || !category) {
-      return res.status(400).send('Todos los campos son obligatorios');
-    }
-
-    try {
-        const data = await fs.readFile(path, 'utf8');
-        const products = JSON.parse(data);
-
-        const newId = (products.length > 0) ? Math.max(...products.map(p => p.id)) + 1 : 1;
-
-        const newProduct = { 
-            id: newId, 
-            title, 
-            description, 
-            code, 
-            price, 
-            status: status || true, 
-            stock, 
-            category,
-            thumbnails: thumbnails || []
-          };
-          
-        products.push(newProduct);
-
-        await fs.writeFile(path, JSON.stringify(products, null, 2));
-
-        res.status(201).json(newProduct);
-    } catch (err) {
-        console.error('Error al guardar el producto:', err);
-        res.status(500).send('Error al guardar el producto');
-    }
-});
-
-module.exports = router;
-
-
-
-router.put('/:pid', async (req, res) => {
-  const { pid } = req.params;
   const { title, description, code, price, status, stock, category, thumbnails } = req.body;
+
+  if (!title || !description || !code || !price || !status || !stock || !category) {
+    return res.status(400).send('Todos los campos son obligatorios');
+  }
+
+
+  let priceAsNumber = parseFloat(price);
+  if (isNaN(priceAsNumber)) {
+    return res.status(400).send('El precio debe ser un número válido');
+  }
+
   try {
     const data = await fs.readFile(path, 'utf8');
-    let products = JSON.parse(data);
-    const productIndex = products.findIndex(p => p.id == pid);
-    if (productIndex === -1) return res.status(404).send('Producto no encontrado');
-    products[productIndex] = { ...products[productIndex], title, description, code, price, status, stock, category, thumbnails };
+    const products = JSON.parse(data);
+    const newId = (products.length > 0) ? Math.max(...products.map(p => p.id)) + 1 : 1;
+
+    const newProduct = { 
+        id: newId, 
+        title, 
+        description, 
+        code, 
+        price: priceAsNumber,
+        status: status || true, 
+        stock, 
+        category,
+        thumbnails: thumbnails || []
+      };
+    
+    products.push(newProduct);
+
+    products.forEach(product => {
+      product.price = parseFloat(product.price);
+    });
+
     await fs.writeFile(path, JSON.stringify(products, null, 2));
-    res.json(products[productIndex]);
+
+    res.status(201).json(newProduct);
   } catch (err) {
-    res.status(500).send('Error al actualizar el producto');
+    res.status(500).send('Error al guardar el producto');
   }
 });
-
 
 router.delete('/:pid', async (req, res) => {
   const { pid } = req.params;
   try {
     const data = await fs.readFile(path, 'utf8');
     let products = JSON.parse(data);
-    products = products.filter(p => p.id != pid);
+    products = products.filter(p => p.id !== parseInt(pid));
     await fs.writeFile(path, JSON.stringify(products, null, 2));
     res.status(204).send();
   } catch (err) {
